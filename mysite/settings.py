4ca39cbea3ac343cf40276a0a402420e3e7b87c4
RohitC1234 <rohitcharmode2590@gmail.com>
2026-05-24 18:02:31 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,5 +123,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "rohitshbx@gmail.com"
-EMAIL_HOST_PASSWORD = "fhmpariuaercldpa"
+EMAIL_HOST_USER = "youremailid"
+EMAIL_HOST_PASSWORD = "generated_password"
